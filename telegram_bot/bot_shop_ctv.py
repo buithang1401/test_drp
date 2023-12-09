02900e41cc9a8d613fcc40698a646d40fd9fc5f9
buithang1401 <thang.dev.1401@gmail.com>
2023-12-09 08:28:11 +0700

    ok

--- telegram_bot/bot_shop_ctv.py
+++ telegram_bot/bot_shop_ctv.py
@@ -25,6 +25,10 @@ def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
+user_name = 'testuser'
+password = '123465422'
+
 def order(update, context):
     try:
         '''
